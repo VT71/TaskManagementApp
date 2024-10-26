1c366cb65706b3c7b2ad2bb3be23089e51ef28a2
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-26 19:27:00 +0100

    Temporarily commented out action methods pending implementation

--- TaskService/Controllers/ToDoTaskController.cs
+++ TaskService/Controllers/ToDoTaskController.cs
@@ -23,80 +23,80 @@ namespace TaskService.Controllers
         }
 
         // GET: api/ToDoTask/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<ToDoTask>> GetToDoTask(long id)
-        {
-            var toDoTask = await _context.ToDoTasks.FindAsync(id);
+        // [HttpGet("{id}")]
+        // public async Task<ActionResult<ToDoTask>> GetToDoTask(long id)
+        // {
+        //     var toDoTask = await _context.ToDoTasks.FindAsync(id);
 
-            if (toDoTask == null)
-            {
-                return NotFound();
-            }
+        //     if (toDoTask == null)
+        //     {
+        //         return NotFound();
+        //     }
 
-            return toDoTask;
-        }
+        //     return toDoTask;
+        // }
 
         // PUT: api/ToDoTask/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutToDoTask(long id, ToDoTask toDoTask)
-        {
-            if (id != toDoTask.Id)
-            {
-                return BadRequest();
-            }
+        // [HttpPut("{id}")]
+        // public async Task<IActionResult> PutToDoTask(long id, ToDoTask toDoTask)
+        // {
+        //     if (id != toDoTask.Id)
+        //     {
+        //         return BadRequest();
+        //     }
 
-            _context.Entry(toDoTask).State = EntityState.Modified;
+        //     _context.Entry(toDoTask).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ToDoTaskExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //     try
+        //     {
+        //         await _context.SaveChangesAsync();
+        //     }
+        //     catch (DbUpdateConcurrencyException)
+        //     {
+        //         if (!ToDoTaskExists(id))
+        //         {
+        //             return NotFound();
+        //         }
+        //         else
+        //         {
+        //             throw;
+        //         }
+        //     }
 
-            return NoContent();
-        }
+        //     return NoContent();
+        // }
 
         // POST: api/ToDoTask
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<ToDoTask>> PostToDoTask(ToDoTask toDoTask)
-        {
-            _context.ToDoTasks.Add(toDoTask);
-            await _context.SaveChangesAsync();
+        // [HttpPost]
+        // public async Task<ActionResult<ToDoTask>> PostToDoTask(ToDoTask toDoTask)
+        // {
+        //     _context.ToDoTasks.Add(toDoTask);
+        //     await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetToDoTask", new { id = toDoTask.Id }, toDoTask);
-        }
+        //     return CreatedAtAction("GetToDoTask", new { id = toDoTask.Id }, toDoTask);
+        // }
 
         // DELETE: api/ToDoTask/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteToDoTask(long id)
-        {
-            var toDoTask = await _context.ToDoTasks.FindAsync(id);
-            if (toDoTask == null)
-            {
-                return NotFound();
-            }
+        // [HttpDelete("{id}")]
+        // public async Task<IActionResult> DeleteToDoTask(long id)
+        // {
+        //     var toDoTask = await _context.ToDoTasks.FindAsync(id);
+        //     if (toDoTask == null)
+        //     {
+        //         return NotFound();
+        //     }
 
-            _context.ToDoTasks.Remove(toDoTask);
-            await _context.SaveChangesAsync();
+        //     _context.ToDoTasks.Remove(toDoTask);
+        //     await _context.SaveChangesAsync();
 
-            return NoContent();
-        }
+        //     return NoContent();
+        // }
 
-        private bool ToDoTaskExists(long id)
-        {
-            return _context.ToDoTasks.Any(e => e.Id == id);
-        }
+        // private bool ToDoTaskExists(long id)
+        // {
+        //     return _context.ToDoTasks.Any(e => e.Id == id);
+        // }
     }
 }
