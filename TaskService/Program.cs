935d86e255e9ed8e786db01997a9caa0e91d9067
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-26 18:55:21 +0100

    Registered the database context

--- TaskService/Program.cs
+++ TaskService/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TaskService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TaskContext>(opt =>
+    opt.UseSqlite("Data Source=databse.dat"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
