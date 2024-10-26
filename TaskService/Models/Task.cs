d83dac9fc64ef988697eb604a771d7b1b8b95544
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-26 17:47:59 +0100

    Added title validation attribute for max length

--- TaskService/Models/Task.cs
+++ TaskService/Models/Task.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TaskService.Models;
 
 public class Task
 {
     public long Id { get; set; }
+    [StringLength(100)]
     public required string Title { get; set; }
     public string? Description { get; set; }
     public required DateTimeOffset DueDate { get; set; }
