5b2623f1ff4cc19975169c9734748ee9ad6fa3f2
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-27 16:54:06 +0000

    Added data seeding for the local database

--- TaskService/Data/ToDoTaskContext.cs
+++ TaskService/Data/ToDoTaskContext.cs
@@ -9,5 +9,13 @@ public class ToDoTaskContext : DbContext
         : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ToDoTask>().HasData(
+            new ToDoTask { Id = 1, Title = "Task 1", DueDate = DateTime.Parse("2025-10-27T15:23:59.689Z") },
+            new ToDoTask { Id = 2, Title = "Task 2", DueDate = DateTime.Parse("2025-10-27T15:23:59.689Z") },
+            new ToDoTask { Id = 3, Title = "Task 3", DueDate = DateTime.Parse("2025-10-27T15:23:59.689Z") });
+    }
     public virtual DbSet<ToDoTask> ToDoTasks { get; set; }
 }
---
+++ TaskService/Migrations/20241027165158_DataSeedAdded.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TaskService.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeedAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ToDoTasks",
+                columns: new[] { "Id", "Completed", "Description", "DueDate", "Title" },
+                values: new object[,]
+                {
+                    { 1L, false, null, new DateTime(2025, 10, 27, 15, 23, 59, 689, DateTimeKind.Local), "Task 1" },
+                    { 2L, false, null, new DateTime(2025, 10, 27, 15, 23, 59, 689, DateTimeKind.Local), "Task 2" },
+                    { 3L, false, null, new DateTime(2025, 10, 27, 15, 23, 59, 689, DateTimeKind.Local), "Task 3" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ToDoTasks",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "ToDoTasks",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "ToDoTasks",
+                keyColumn: "Id",
+                keyValue: 3L);
+        }
+    }
+}
