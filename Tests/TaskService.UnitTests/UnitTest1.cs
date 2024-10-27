692e6511985dd2aebfdf12ed3fcc031d4847c5c8
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-27 18:41:29 +0000

    Added a test for checking the correct amount of tasks is returned when there are more than 1 task, when requesting all

--- Tests/TaskService.UnitTests/UnitTest1.cs
+++ Tests/TaskService.UnitTests/UnitTest1.cs
@@ -57,4 +57,36 @@ public class ToDoTaskControllerTests
         var tasks = Assert.IsAssignableFrom<List<ToDoTask>>(okResult.Value);
         Assert.Single(tasks);
     }
+    [Fact]
+    public async Task GetAllTasksReturnsCorrectAmountMultiple()
+    {
+        var dbContextMock = new Mock<ToDoTaskContext>(new DbContextOptions<ToDoTaskContext>());
+
+        ToDoTaskService toDoTaskService = new ToDoTaskService(dbContextMock.Object);
+        ToDoTaskController toDoTaskController = new ToDoTaskController(toDoTaskService);
+
+        var tasksSet = new List<ToDoTask>()
+        {
+            new ToDoTask
+            {
+                Id = 1,
+                Title = "Task 1",
+                DueDate = DateTime.Parse("2025-10-27T15:23:59.689Z")
+            },
+            new ToDoTask
+            {
+                Id = 2,
+                Title = "Task 2",
+                DueDate = DateTime.Parse("2025-10-27T15:23:59.689Z")
+            }
+        };
+
+        dbContextMock.Setup(x => x.ToDoTasks).ReturnsDbSet(tasksSet);
+
+        var allToDoTasks = await toDoTaskController.GetToDoTasks();
+
+        var okResult = Assert.IsType<OkObjectResult>(allToDoTasks.Result);
+        var tasks = Assert.IsAssignableFrom<List<ToDoTask>>(okResult.Value);
+        Assert.Equal(2, tasks.Count);
+    }
 }
