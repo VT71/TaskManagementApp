3016d91927ba20a879837b6468b74c5cca893cb3
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-28 18:10:06 +0000

    Task Table added to the Task List Page Component

--- ClientApp/src/app/components/task-list-page/task-list-page.component.ts
+++ ClientApp/src/app/components/task-list-page/task-list-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TaskTableComponent } from '../task-table/task-table.component';
 
 @Component({
   selector: 'app-task-list-page',
   standalone: true,
-  imports: [],
+  imports: [TaskTableComponent],
   templateUrl: './task-list-page.component.html',
   styleUrl: './task-list-page.component.css'
 })
