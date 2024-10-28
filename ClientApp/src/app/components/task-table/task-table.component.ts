3f389fb085b3f47259ce8feafd1a9a74d55f2d87
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-28 21:45:04 +0000

    Added error handling when getting Tasks data for the table + snackbar for message display

--- ClientApp/src/app/components/task-table/task-table.component.ts
+++ ClientApp/src/app/components/task-table/task-table.component.ts
@@ -8,6 +8,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { ToDoTask } from '../../interfaces/to-do-task';
 import { TodotasksApiService } from '../../services/todotasks-api.service';
 import { Subscription } from 'rxjs';
+import {
+    MatSnackBar, MatSnackBarHorizontalPosition,
+    MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
     selector: 'app-task-table',
@@ -16,6 +20,7 @@ import { Subscription } from 'rxjs';
     templateUrl: './task-table.component.html',
     styleUrl: './task-table.component.css'
 })
+
 export class TaskTableComponent implements AfterViewInit, OnInit, OnDestroy {
     displayedColumns: string[] = ['id', 'title', 'description', 'dueDate', 'completed'];
     dataSource: MatTableDataSource<ToDoTask> = new MatTableDataSource();
@@ -24,22 +29,21 @@ export class TaskTableComponent implements AfterViewInit, OnInit, OnDestroy {
     @ViewChild(MatSort) sort!: MatSort;
 
     private subscriptions: Subscription[] = [];
+
     private toDoTasksApiService = inject(TodotasksApiService);
     public toDoTasks: ToDoTask[] = [];
 
-    constructor() {
-        // const tasks: ToDoTask[] = [{
-        //     id: 1, title: "Task 1", description: null, dueDate: new Date("2025-10-27T15:23:59.689Z"), completed: false
-        // }]
-
-        // this.dataSource = new MatTableDataSource(tasks);
-    }
+    private _snackBar = inject(MatSnackBar);
+    horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+    verticalPosition: MatSnackBarVerticalPosition = 'top';
 
     ngOnInit(): void {
         this.subscriptions.push(
-            this.toDoTasksApiService.getAllTasks().subscribe((toDoTasks) => {
-                this.dataSource = new MatTableDataSource(toDoTasks);
-            })
+            this.toDoTasksApiService.getAllTasks().subscribe(
+                {
+                    next: (toDoTasks) => this.dataSource = new MatTableDataSource(toDoTasks),
+                    error: (e) => this.openSnackBar("Error occured when getting Tasks Data")
+                })
         )
     }
 
@@ -50,6 +54,15 @@ export class TaskTableComponent implements AfterViewInit, OnInit, OnDestroy {
 
     ngOnDestroy(): void {
         this.subscriptions.forEach((subcription) => subcription.unsubscribe);
+    }
+
+    openSnackBar(message: string) {
+        let durationInSeconds = 5;
+        this._snackBar.open(message, '', {
+            horizontalPosition: this.horizontalPosition,
+            verticalPosition: this.verticalPosition,
+            duration: durationInSeconds * 1000
+        });
     }
 
     applyFilter(event: Event) {
