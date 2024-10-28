b6a7b94ee8dc70afb126f7ca1b119707551bc987
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-28 17:12:41 +0000

    Connected the root route with the Task List Page component

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TaskListPageComponent } from './components/task-list-page/task-list-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: TaskListPageComponent },
+];
