d2e53a2c2b505bb2e7e6ec5afbcbd70af56436cc
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-29 18:11:15 +0000

    Added the Task Managing Form to the Edit Task page

--- ClientApp/src/app/components/edit-task-page/edit-task-page.component.ts
+++ ClientApp/src/app/components/edit-task-page/edit-task-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { TaskManageFormComponent } from '../task-manage-form/task-manage-form.component';
 
 @Component({
-  selector: 'app-edit-task-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './edit-task-page.component.html',
-  styleUrl: './edit-task-page.component.css'
+    selector: 'app-edit-task-page',
+    standalone: true,
+    imports: [TaskManageFormComponent],
+    templateUrl: './edit-task-page.component.html',
+    styleUrl: './edit-task-page.component.css'
 })
 export class EditTaskPageComponent {
 
