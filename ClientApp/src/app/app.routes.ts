8e508aee1a3a3b39c15c67338a17b4d1d5a9140e
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-29 17:41:09 +0000

    Created a route with a parameter for the Edit Task Page component

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { TaskListPageComponent } from './components/task-list-page/task-list-page.component';
 import { AddTaskPageComponent } from './components/add-task-page/add-task-page.component';
+import { EditTaskPageComponent } from './components/edit-task-page/edit-task-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/tasks', pathMatch: "full" },
     { path: 'tasks', component: TaskListPageComponent, },
-    { path: 'tasks/add-task', component: AddTaskPageComponent }
+    { path: 'tasks/add-task', component: AddTaskPageComponent },
+    { path: 'tasks/edit-task/:id', component: EditTaskPageComponent }
 ];
