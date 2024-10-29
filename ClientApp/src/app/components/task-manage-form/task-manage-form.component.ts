501cf0d8e9440774c82b07727306809d650a628e
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-29 11:54:09 +0000

    Task Managing Form: input added for the task title

--- ClientApp/src/app/components/task-manage-form/task-manage-form.component.ts
+++ ClientApp/src/app/components/task-manage-form/task-manage-form.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
-  selector: 'app-task-manage-form',
-  standalone: true,
-  imports: [],
-  templateUrl: './task-manage-form.component.html',
-  styleUrl: './task-manage-form.component.css'
+    selector: 'app-task-manage-form',
+    standalone: true,
+    imports: [MatFormFieldModule, MatInputModule],
+    templateUrl: './task-manage-form.component.html',
+    styleUrl: './task-manage-form.component.css'
 })
 export class TaskManageFormComponent {
 
