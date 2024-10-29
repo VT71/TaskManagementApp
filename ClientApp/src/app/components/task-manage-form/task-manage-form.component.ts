75b8f339d15d8f009f81c2bf6f165b1fcb631ff7
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-29 13:44:10 +0000

    Task Managing Form : Custom validator added for due date, to be in the future only

--- ClientApp/src/app/components/task-manage-form/task-manage-form.component.ts
+++ ClientApp/src/app/components/task-manage-form/task-manage-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
@@ -27,6 +27,26 @@ export class TaskManageFormComponent {
         description: [''],
         dueDate: [new Date(), [Validators.required]],
         completed: [false, [Validators.required]]
-    })
+    },
+        { validators: this.futureDateValidator })
 
+    private futureDateValidator(control: AbstractControl): ValidationErrors | null {
+        const dueDateInput = control.get('dueDate');
+        const dueDateTimeObject = new Date(dueDateInput?.value);
+
+        const todayDateTime = new Date();
+
+        let errors = {
+            pickUpDateTime: { error: false },
+            dropOffDateTime: { error: false },
+        };
+
+        if (dueDateTimeObject <= todayDateTime) {
+            control.get('dueDate')?.setErrors({ invalidDate: true });
+        }
+
+        return dueDateTimeObject <= todayDateTime
+            ? { invalidDueDate: { value: control.value } }
+            : null;
+    }
 }
