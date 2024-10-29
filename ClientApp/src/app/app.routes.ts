903a950f4a75dde0910608e08ff5fe2f99af6d9d
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-29 11:13:50 +0000

    Created a separate route for task list

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { TaskListPageComponent } from './components/task-list-page/task-list-page.component';
 
 export const routes: Routes = [
-    { path: '', component: TaskListPageComponent },
+    { path: '', redirectTo: '/tasks', pathMatch: "full" },
+    { path: 'tasks', component: TaskListPageComponent },
 ];
