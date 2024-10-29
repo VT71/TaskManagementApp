58226f5b802bf82eb254e1895997eb1e38333389
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-29 12:29:22 +0000

    Added client-side form validation for the title input

--- ClientApp/src/app/components/task-manage-form/task-manage-form.component.ts
+++ ClientApp/src/app/components/task-manage-form/task-manage-form.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
@@ -15,10 +15,15 @@ import { MatButtonModule } from '@angular/material/button';
         { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
         provideNativeDateAdapter(),
     ],
-    imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatSelectModule, MatButtonModule],
+    imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatSelectModule, MatButtonModule, ReactiveFormsModule],
     templateUrl: './task-manage-form.component.html',
     styleUrl: './task-manage-form.component.css'
 })
 export class TaskManageFormComponent {
+    private formBuilder = inject(FormBuilder);
+
+    public formGroup = this.formBuilder.group({
+        title: ['', [Validators.required, Validators.maxLength(100)]]
+    })
 
 }
