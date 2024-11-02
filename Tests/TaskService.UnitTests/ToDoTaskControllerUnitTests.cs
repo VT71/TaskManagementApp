f7b7a7ebb7797f637bd1456ad6f2a61170a59523
VT71 <56217276+VT71@users.noreply.github.com>
2024-11-02 19:01:34 +0200

    TaskService Tests: Changed the unit test checking expected tasks are returned when sorting by title in ascending order, when compared with the context db set.

--- Tests/TaskService.UnitTests/ToDoTaskControllerUnitTests.cs
+++ Tests/TaskService.UnitTests/ToDoTaskControllerUnitTests.cs
@@ -177,19 +177,18 @@ public class ToDoTaskControllerTests
     public async Task GetExpectedTasksWhenSortingByTitleAsc()
     {
         var dbContext = DatabaseContext();
-
+        List<ToDoTask> allToDoTasks = await dbContext.ToDoTasks.OrderBy(task => task.Title).ToListAsync();
+        pageSize = allToDoTasks.Count;
         ToDoTaskService toDoTaskService = new ToDoTaskService(dbContext);
         ToDoTaskController toDoTaskController = new ToDoTaskController(toDoTaskService);
 
-        var filteredToDoTasksResult = await toDoTaskController.GetFilteredToDoTasks(titleSearch: null, sortBy: "title", sortDirection: "asc");
+        var filteredToDoTasksResult = await toDoTaskController.GetFilteredToDoTasks(titleSearch: null, sortBy: "title", sortDirection: "asc",  page: 1, pageSize);
 
         var result = Assert.IsType<OkObjectResult>(filteredToDoTasksResult.Result);
         var pagedUnit = Assert.IsAssignableFrom<PagedUnit<ToDoTask>>(result.Value);
         var filteredToDoTasks = Assert.IsAssignableFrom<List<ToDoTask>>(pagedUnit.Items);
-        Assert.Equal(26, pagedUnit.TotalCount);
-        Assert.Equal("Task A", filteredToDoTasks[0].Title);
-        Assert.Equal("Task B", filteredToDoTasks[1].Title);
-        Assert.Equal("Task C", filteredToDoTasks[2].Title);
+        Assert.Equal(allToDoTasks.Count, pagedUnit.TotalCount);
+        Assert.True(filteredToDoTasks.SequenceEqual(allToDoTasks));
     }
 
     [Fact]
