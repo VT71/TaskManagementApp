86c43fa2ce0d1f4f60cba51fc5dea6a599a2e5a7
VT71 <56217276+VT71@users.noreply.github.com>
2024-11-02 15:46:06 +0200

    TaskService Tests: Created a unit test to check an expected sorted list when sorting by Due Date in ascending order

--- Tests/TaskService.UnitTests/ToDoTaskControllerUnitTests.cs
+++ Tests/TaskService.UnitTests/ToDoTaskControllerUnitTests.cs
@@ -181,6 +181,25 @@ public class ToDoTaskControllerTests
     }
 
     [Fact]
+    public async Task GetExpectedTasksWhenSortingByTitleDesc()
+    {
+        var dbContext = DatabaseContext();
+
+        ToDoTaskService toDoTaskService = new ToDoTaskService(dbContext);
+        ToDoTaskController toDoTaskController = new ToDoTaskController(toDoTaskService);
+
+        var filteredToDoTasksResult = await toDoTaskController.GetFilteredToDoTasks(titleSearch: null, sortBy: "title", sortDirection: "desc");
+
+        var result = Assert.IsType<OkObjectResult>(filteredToDoTasksResult.Result);
+        var pagedUnit = Assert.IsAssignableFrom<PagedUnit<ToDoTask>>(result.Value);
+        var filteredToDoTasks = Assert.IsAssignableFrom<List<ToDoTask>>(pagedUnit.Items);
+        Assert.Equal(26, pagedUnit.TotalCount);
+        Assert.Equal("Task Z", filteredToDoTasks[0].Title);
+        Assert.Equal("Task Y", filteredToDoTasks[1].Title);
+        Assert.Equal("Task X", filteredToDoTasks[2].Title);
+    }
+
+    [Fact]
     public async Task GetNewTaskWhenValidTaskCreated()
     {
         var dbContext = DatabaseContext();
