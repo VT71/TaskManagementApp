e9e0b90c29fc02fdb1187f7404ef942e1ce754ab
VT71 <56217276+VT71@users.noreply.github.com>
2024-11-02 12:58:28 +0200

    Added an auth guard for all routes, including task table, add task and edit task pages.

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { TaskListPageComponent } from './components/task-list-page/task-list-pag
 import { AddTaskPageComponent } from './components/add-task-page/add-task-page.component';
 import { EditTaskPageComponent } from './components/edit-task-page/edit-task-page.component';
 
+import { AuthGuard } from '@auth0/auth0-angular';
+
 export const routes: Routes = [
     { path: '', redirectTo: '/tasks', pathMatch: "full" },
-    { path: 'tasks', component: TaskListPageComponent, },
-    { path: 'tasks/add-task', component: AddTaskPageComponent },
-    { path: 'tasks/edit-task/:id', component: EditTaskPageComponent }
+    { path: 'tasks', component: TaskListPageComponent, canActivate: [AuthGuard] },
+    { path: 'tasks/add-task', component: AddTaskPageComponent, canActivate: [AuthGuard] },
+    { path: 'tasks/edit-task/:id', component: EditTaskPageComponent, canActivate: [AuthGuard] }
 ];
