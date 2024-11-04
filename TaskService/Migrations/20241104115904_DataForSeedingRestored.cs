31ea072de9f3bc742d796d2638690f179b7b368a
VT71 <56217276+VT71@users.noreply.github.com>
2024-11-04 14:03:01 +0200

    TaskService : Data for seeding restored

---
+++ TaskService/Migrations/20241104115904_DataForSeedingRestored.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TaskService.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataForSeedingRestored : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDoTasks",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    DueDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Completed = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoTasks", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ToDoTasks",
+                columns: new[] { "Id", "Completed", "Description", "DueDate", "Title" },
+                values: new object[,]
+                {
+                    { 1L, false, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut velit vel orci dapibus tristique.", new DateTime(2025, 1, 1, 2, 0, 0, 0, DateTimeKind.Local), "Task A" },
+                    { 2L, true, null, new DateTime(2025, 1, 2, 2, 0, 0, 0, DateTimeKind.Local), "Task B" },
+                    { 3L, false, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non orci sit amet turpis cursus.", new DateTime(2025, 1, 3, 2, 0, 0, 0, DateTimeKind.Local), "Task C" },
+                    { 4L, true, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a massa nec elit vestibulum aliquam.", new DateTime(2025, 1, 4, 2, 0, 0, 0, DateTimeKind.Local), "Task D" },
+                    { 5L, false, null, new DateTime(2025, 1, 5, 2, 0, 0, 0, DateTimeKind.Local), "Task E" },
+                    { 6L, true, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi volutpat tortor et magna bibendum.", new DateTime(2025, 1, 6, 2, 0, 0, 0, DateTimeKind.Local), "Task F" },
+                    { 7L, false, null, new DateTime(2025, 1, 7, 2, 0, 0, 0, DateTimeKind.Local), "Task G" },
+                    { 8L, true, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a risus ac sapien ultricies.", new DateTime(2025, 1, 8, 2, 0, 0, 0, DateTimeKind.Local), "Task H" },
+                    { 9L, false, null, new DateTime(2025, 1, 9, 2, 0, 0, 0, DateTimeKind.Local), "Task I" },
+                    { 10L, true, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula nisl at elit feugiat.", new DateTime(2025, 1, 10, 2, 0, 0, 0, DateTimeKind.Local), "Task J" },
+                    { 11L, false, null, new DateTime(2025, 1, 11, 2, 0, 0, 0, DateTimeKind.Local), "Task K" },
+                    { 12L, true, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur auctor orci sit amet mi.", new DateTime(2025, 1, 12, 2, 0, 0, 0, DateTimeKind.Local), "Task L" },
+                    { 13L, false, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor ligula eget cursus.", new DateTime(2025, 1, 13, 2, 0, 0, 0, DateTimeKind.Local), "Task M" },
+                    { 14L, true, null, new DateTime(2025, 1, 14, 2, 0, 0, 0, DateTimeKind.Local), "Task N" },
+                    { 15L, false, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus scelerisque orci vitae metus.", new DateTime(2025, 1, 15, 2, 0, 0, 0, DateTimeKind.Local), "Task O" },
+                    { 16L, true, null, new DateTime(2025, 1, 16, 2, 0, 0, 0, DateTimeKind.Local), "Task P" },
+                    { 17L, false, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at justo vel erat pharetra.", new DateTime(2025, 1, 17, 2, 0, 0, 0, DateTimeKind.Local), "Task Q" },
+                    { 18L, true, null, new DateTime(2025, 1, 18, 2, 0, 0, 0, DateTimeKind.Local), "Task R" },
+                    { 19L, false, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et lacus pulvinar, porta eros.", new DateTime(2025, 1, 19, 2, 0, 0, 0, DateTimeKind.Local), "Task S" },
+                    { 20L, true, null, new DateTime(2025, 1, 20, 2, 0, 0, 0, DateTimeKind.Local), "Task T" },
+                    { 21L, false, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet lacus lacinia.", new DateTime(2025, 1, 21, 2, 0, 0, 0, DateTimeKind.Local), "Task U" },
+                    { 22L, true, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non justo at orci commodo.", new DateTime(2025, 1, 22, 2, 0, 0, 0, DateTimeKind.Local), "Task V" },
+                    { 23L, false, null, new DateTime(2025, 1, 23, 2, 0, 0, 0, DateTimeKind.Local), "Task W" },
+                    { 24L, true, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consectetur orci id odio.", new DateTime(2025, 1, 24, 2, 0, 0, 0, DateTimeKind.Local), "Task X" },
+                    { 25L, false, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed felis vestibulum, lacinia.", new DateTime(2025, 1, 25, 2, 0, 0, 0, DateTimeKind.Local), "Task Y" },
+                    { 26L, true, null, new DateTime(2025, 1, 26, 2, 0, 0, 0, DateTimeKind.Local), "Task Z" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoTasks");
+        }
+    }
+}
--- TaskService/Migrations/ToDoTaskContextModelSnapshot.cs
+++ TaskService/Migrations/ToDoTaskContextModelSnapshot.cs
@@ -47,14 +47,14 @@ namespace TaskService.Migrations
                             Id = 1L,
                             Completed = false,
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut velit vel orci dapibus tristique.",
-                            DueDate = new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 1, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task A"
                         },
                         new
                         {
                             Id = 2L,
                             Completed = true,
-                            DueDate = new DateTime(2025, 1, 2, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 2, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task B"
                         },
                         new
@@ -62,7 +62,7 @@ namespace TaskService.Migrations
                             Id = 3L,
                             Completed = false,
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non orci sit amet turpis cursus.",
-                            DueDate = new DateTime(2025, 1, 3, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 3, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task C"
                         },
                         new
@@ -70,14 +70,14 @@ namespace TaskService.Migrations
                             Id = 4L,
                             Completed = true,
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a massa nec elit vestibulum aliquam.",
-                            DueDate = new DateTime(2025, 1, 4, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 4, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task D"
                         },
                         new
                         {
                             Id = 5L,
                             Completed = false,
-                            DueDate = new DateTime(2025, 1, 5, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 5, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task E"
                         },
                         new
@@ -85,14 +85,14 @@ namespace TaskService.Migrations
                             Id = 6L,
                             Completed = true,
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi volutpat tortor et magna bibendum.",
-                            DueDate = new DateTime(2025, 1, 6, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 6, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task F"
                         },
                         new
                         {
                             Id = 7L,
                             Completed = false,
-                            DueDate = new DateTime(2025, 1, 7, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 7, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task G"
                         },
                         new
@@ -100,14 +100,14 @@ namespace TaskService.Migrations
                             Id = 8L,
                             Completed = true,
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a risus ac sapien ultricies.",
-                            DueDate = new DateTime(2025, 1, 8, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 8, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task H"
                         },
                         new
                         {
                             Id = 9L,
                             Completed = false,
-                            DueDate = new DateTime(2025, 1, 9, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 9, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task I"
                         },
                         new
@@ -115,14 +115,14 @@ namespace TaskService.Migrations
                             Id = 10L,
                             Completed = true,
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula nisl at elit feugiat.",
-                            DueDate = new DateTime(2025, 1, 10, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 10, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task J"
                         },
                         new
                         {
                             Id = 11L,
                             Completed = false,
-                            DueDate = new DateTime(2025, 1, 11, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 11, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task K"
                         },
                         new
@@ -130,7 +130,7 @@ namespace TaskService.Migrations
                             Id = 12L,
                             Completed = true,
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur auctor orci sit amet mi.",
-                            DueDate = new DateTime(2025, 1, 12, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 12, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task L"
                         },
                         new
@@ -138,14 +138,14 @@ namespace TaskService.Migrations
                             Id = 13L,
                             Completed = false,
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor ligula eget cursus.",
-                            DueDate = new DateTime(2025, 1, 13, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 13, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task M"
                         },
                         new
                         {
                             Id = 14L,
                             Completed = true,
-                            DueDate = new DateTime(2025, 1, 14, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 14, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task N"
                         },
                         new
@@ -153,14 +153,14 @@ namespace TaskService.Migrations
                             Id = 15L,
                             Completed = false,
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus scelerisque orci vitae metus.",
-                            DueDate = new DateTime(2025, 1, 15, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 15, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task O"
                         },
                         new
                         {
                             Id = 16L,
                             Completed = true,
-                            DueDate = new DateTime(2025, 1, 16, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 16, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task P"
                         },
                         new
@@ -168,14 +168,14 @@ namespace TaskService.Migrations
                             Id = 17L,
                             Completed = false,
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at justo vel erat pharetra.",
-                            DueDate = new DateTime(2025, 1, 17, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 17, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task Q"
                         },
                         new
                         {
                             Id = 18L,
                             Completed = true,
-                            DueDate = new DateTime(2025, 1, 18, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 18, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task R"
                         },
                         new
@@ -183,14 +183,14 @@ namespace TaskService.Migrations
                             Id = 19L,
                             Completed = false,
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et lacus pulvinar, porta eros.",
-                            DueDate = new DateTime(2025, 1, 19, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 19, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task S"
                         },
                         new
                         {
                             Id = 20L,
                             Completed = true,
-                            DueDate = new DateTime(2025, 1, 20, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 20, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task T"
                         },
                         new
@@ -198,7 +198,7 @@ namespace TaskService.Migrations
                             Id = 21L,
                             Completed = false,
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet lacus lacinia.",
-                            DueDate = new DateTime(2025, 1, 21, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 21, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task U"
                         },
                         new
@@ -206,14 +206,14 @@ namespace TaskService.Migrations
                             Id = 22L,
                             Completed = true,
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non justo at orci commodo.",
-                            DueDate = new DateTime(2025, 1, 22, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 22, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task V"
                         },
                         new
                         {
                             Id = 23L,
                             Completed = false,
-                            DueDate = new DateTime(2025, 1, 23, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 23, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task W"
                         },
                         new
@@ -221,7 +221,7 @@ namespace TaskService.Migrations
                             Id = 24L,
                             Completed = true,
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consectetur orci id odio.",
-                            DueDate = new DateTime(2025, 1, 24, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 24, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task X"
                         },
                         new
@@ -229,14 +229,14 @@ namespace TaskService.Migrations
                             Id = 25L,
                             Completed = false,
                             Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed felis vestibulum, lacinia.",
-                            DueDate = new DateTime(2025, 1, 25, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 25, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task Y"
                         },
                         new
                         {
                             Id = 26L,
                             Completed = true,
-                            DueDate = new DateTime(2025, 1, 26, 0, 0, 0, 0, DateTimeKind.Local),
+                            DueDate = new DateTime(2025, 1, 26, 2, 0, 0, 0, DateTimeKind.Local),
                             Title = "Task Z"
                         });
                 });
